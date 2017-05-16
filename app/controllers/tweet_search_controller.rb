c425f3d9969136f8ac8b5d30fb9ed020da4c18d1
Robert Gogolok <gogolok@gmail.com>
2017-05-16 15:02:30 +0200

    simplify code

--- app/controllers/tweet_search_controller.rb
+++ app/controllers/tweet_search_controller.rb
@@ -8,8 +8,8 @@ class TweetSearchController < ApplicationController
 
   def results
     query = params[:query]
-    @text_results = HTTParty.get("#{elasticsearch_url}/twitter/tweet/_search?pretty&size=10", basic_auth: elasticsearch_auth, body: {"query":{"match_phrase": {"message": '.*' + query + '.*' }}}.to_json)
-    @name_results = HTTParty.get("#{elasticsearch_url}/twitter/tweet/_search?pretty&size=10", basic_auth: elasticsearch_auth, body: {"query":{"match_phrase": {"user": "#{query}" }}}.to_json)
+    @text_results = HTTParty.get("#{es_url}/twitter/tweet/_search?pretty&size=10", es_default_options.merge({body: {"query":{"match_phrase": {"message": '.*' + query + '.*' }}}.to_json}))
+    @name_results = HTTParty.get("#{es_url}/twitter/tweet/_search?pretty&size=10", es_default_options.merge({body: {"query":{"match_phrase": {"user": "#{query}" }}}.to_json}))
   end
 
   def seed
@@ -18,7 +18,7 @@ class TweetSearchController < ApplicationController
 
       users.each do |user|
         10.times do
-          HTTParty.post( "#{elasticsearch_url.first}/twitter/tweet", basic_auth: elasticsearch_auth, body: {"user" => "#{user}", "message": "#{FFaker::BaconIpsum.sentence}"}.to_json)
+          HTTParty.post("#{es_url}/twitter/tweet", es_default_options.merge({body: {"user" => "#{user}", "message": "#{FFaker::BaconIpsum.sentence}"}.to_json}))
         end
       end
       flash[:success] = "Seeded Elasticsearch"
@@ -42,14 +42,19 @@ class TweetSearchController < ApplicationController
     end
   end
 
-  def elasticsearch_url
+  def es_url
     credentials['host'].first
   end
 
-  def elasticsearch_auth
+  def es_auth
     { username: credentials['username'], password: credentials['password'] }
+  end
+
+  def es_default_options
+    {basic_auth: es_auth}
   end
 end
 
 class SeedData
+
 end
